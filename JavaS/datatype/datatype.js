46aca2786e7c0fdef4bc62d75a014d50ee5d661c
paudelrajan007 <rajan11paudel@gmail.com>
2026-07-26 16:31:15 +0545

    datatype

---
+++ JavaS/datatype/datatype.js
@@ -0,0 +1,11 @@
+let num = 944;
+let name = "raj";
+const country = "usa";
+let usaexist = true;
+const name3 = Symbol("raj");
+let name2 = Symbol("raj");
+
+console.log(name3 === name2);
+
+let newnum = null;
+let sum = undefined;
---
+++ JavaS/var/var.js
@@ -0,0 +1,6 @@
+const name = "Rajan";
+let phonenum = "939333";
+var city = "Ktm";
+console.log(name, phonenum, city);
+
+console.table([name, phonenum, city]);
