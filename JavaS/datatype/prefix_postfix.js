53afdf284c3d24b568dbe69cab925825b658aa6b
paudelrajan007 <rajan11paudel@gmail.com>
2026-07-27 22:17:19 +0545

    prefix_postfix

---
+++ JavaS/datatype/prefix_postfix.js
@@ -0,0 +1,20 @@
+// let a = 10;
+// let c = a++;
+// console.log(a);
+// console.log(c);
+// let b = ++a;
+// console.log(b);
+
+// console.log(a);
+
+let x = 8;
+
+let y = ++x;
+
+console.log(x, y);
+
+let a = 8;
+
+let b = a++;
+
+console.log(a, b);
